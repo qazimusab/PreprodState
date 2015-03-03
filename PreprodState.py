21ddb7a807ea2591e6191e213f1a6b2d2903b4ad
qa185001 <qazu.ahmed@ncr.com>
2015-03-03 16:24:05 -0500

    latest and greatest

--- PreprodState.py
+++ PreprodState.py
@@ -1,9 +1,19 @@
 __author__ = 'qa185001'
 from flask import Flask
-from flask import jsonify, request, abort
-
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-isPreprodDown = False
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
 
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
